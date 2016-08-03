bbd0c72fcd7edce0c909260307f256a54d0bdf24
dashccc <dashccc@gmail.com>
2016-08-03 14:59:08 +1000

    added test file

---
+++ SkipListTest.py
@@ -0,0 +1,15 @@
+'''
+Created on 3 Aug 2016
+
+@author: Wanyu Yin
+@version: Python 2.7
+'''
+import mock
+import unittest
+from skiplist import SkipList
+
+class SkipListTest(unittest.TestCase):
+    def setUp(self):
+        self.skiplist = SkipList()
+        
+
