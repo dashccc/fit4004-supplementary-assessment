dd8511ee5d2f1f50e2329eaa2c5c739bc6e6c7f0
dashccc <dashccc@gmail.com>
2016-08-08 00:18:13 +1000

    made some changes to the example code

--- part2/example_path_reader.py
+++ part2/example_path_reader.py
@@ -12,13 +12,10 @@ class PathReader:
         self.srcPath = srcPath
         self.filename = ''
         self.extension = ''
-        
-    def get_srcPath(self):
-        return self.srcPath
     
     def get_filename(self):
-        if self.split_srcPath():
-            str_list = self.split_srcPath()
+        if self.is_valid():
+            str_list = self.srcPath.split('.')
             for i in range(len(str_list)-1):
                 self.filename = self.filename + str_list[i] + '.'
             if self.filename[-1] == '.':
@@ -26,17 +23,16 @@ class PathReader:
         return self.filename
     
     def get_extension(self):
-        if self.split_srcPath():
-            str_list = self.split_srcPath()
+        if self.is_valid():
+            str_list = self.srcPath.split('.')
             self.extension = str_list[-1]
         return self.extension
         
-    def split_srcPath(self):
+    def is_valid(self):
         if self.srcPath is None or len(self.srcPath)==0 or len(self.srcPath.split('.'))<2:
-            print 'Invalid source path...'
             return False
         else:
-            return self.srcPath.split('.')
+            return True
 
 if __name__ == '__main__':
     pr = PathReader('aaa.bbb.jpg')
